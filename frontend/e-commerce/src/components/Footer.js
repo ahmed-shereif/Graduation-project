729ae0baa6a7ce08e28c544bea82b3b57dc199bd
ahmed-shereif <ahmed1996sherif@gmail.com>
2022-05-23 00:54:04 +0200

    adding bootstrap fontawsome navbar

---
+++ frontend/e-commerce/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+function Footer() {
+    return (
+        <div>
+            <Container className='text-center fw-bold'>
+
+                Copyrighr &copy; UI-Shop
+            </Container >
+        </div>
+    )
+}
+
+export default Footer
